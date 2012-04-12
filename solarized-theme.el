c1d3c901afa8f9beffd23e05a97ca4f1a8baa93c
Thomas Frössman <thomasf@jossystem.se>
2012-04-12 20:45:56 +0200

    Removed reduntant color definition for org-agenda-date-today

--- solarized-theme.el
+++ solarized-theme.el
@@ -326,10 +326,10 @@
      `(nav-face-hfile ((,class (:foreground ,red))))
 
      ;; org-mode
-     `(org-agenda-date-today
-       ((,class (:foreground ,solarized-fg :slant italic :weight bold))) t)
      `(org-agenda-structure
        ((,class (:inherit font-lock-comment-face :foreground ,solarized-fg))))
+     `(org-agenda-date-today
+       ((,class (:slant italic :weight bold))) t)
      `(org-archived ((,class (:foreground ,solarized-fg :weight bold))))
      `(org-checkbox ((,class (:background ,solarized-bg :foreground ,solarized-fg
                                           :box (:line-width 1 :style released-button)))))
