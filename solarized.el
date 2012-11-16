75fe4a8aa9d3c944febce1032bd4b8adb698ce65
Thomas Frössman <thomasf@jossystem.se>
2012-11-16 20:46:57 +0100

    add compilation mode faces (recently added to emacs 24.3?)

--- solarized.el
+++ solarized.el
@@ -147,6 +147,10 @@
      `(compilation-leave-directory-face ((,class (:foreground ,green))))
      `(compilation-line-face ((,class (:foreground ,yellow))))
      `(compilation-line-number ((,class (:foreground ,yellow))))
+     `(compilation-mode-line-exit
+       ((,class (:inherit compilation-info :foreground ,green :weight bold))))
+     `(compilation-mode-line-fail
+       ((,class (:inherit compilation-error :foreground ,red :weight bold))))
      `(compilation-message-face ((,class (:foreground ,blue))))
      `(compilation-warning-face ((,class (:foreground ,yellow :weight bold :underline t))))
 
