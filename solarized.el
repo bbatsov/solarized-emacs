618e36f2d13f5965634b4b5bbb450024c477e5cf
Thomas Frössman <thomasf@jossystem.se>
2013-07-19 15:18:36 +0200

    add underline instead of box color for inactive mode line #71

--- solarized.el
+++ solarized.el
@@ -251,10 +251,10 @@ customize the resulting theme."
      `(mode-line-buffer-id ((,class (:foreground ,solarized-emph :weight bold))))
      `(mode-line-inactive
        ((,class (:inverse-video unspecified
-                                :underline unspecified
+                                :underline ,solarized-comments
                                 :foreground ,solarized-comments
                                 :background ,solarized-bg
-                                :box (:line-width 1 :color ,solarized-hl
+                                :box (:line-width 1 :color ,solarized-bg
                                                   :style unspecified)))))
      `(header-line
        ((,class (:inverse-video unspecified
