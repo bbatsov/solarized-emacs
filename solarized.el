b7c14117395f7be5f3b77a894c938fed0365193f
Thomas Frössman <thomasf@jossystem.se>
2015-02-05 18:52:54 +0100

    Bump version: 1.2.0 → 1.2.1

--- solarized.el
+++ solarized.el
@@ -5,7 +5,7 @@
 ;; Author: Bozhidar Batsov <bozhidar@batsov.com>
 ;; Author: Thomas Frössman <thomasf@jossystem.se>
 ;; URL: http://github.com/bbatsov/solarized-emacs
-;; Version: 1.2.0
+;; Version: 1.2.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
