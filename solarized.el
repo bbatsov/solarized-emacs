456fe277f078598581a4287c49839d3c9fea0783
Thomas Frössman <thomasf@jossystem.se>
2015-04-21 12:31:19 +0200

    Tweak colors for sx-mode a bit further

--- solarized.el
+++ solarized.el
@@ -1682,24 +1682,29 @@ customize the resulting theme."
                              :weight ,s-maybe-bold))))
 
 ;;;;; sx (WIP)
-     ;; `(sx-custom-button ((,class (:background "lightgrey" :foreground "black" :box (:line-width 3 :style released-button) :height 0.9))))
+     `(sx-custom-button ((,class (:background ,base02 :foreground ,base1
+                                              :box (:line-width 2 :style released-button :height 0.9)))))
      `(sx-question-list-answers ((,class (:inherit sx-question-list-parent :foreground ,green :height 1.0))))
      `(sx-question-list-answers-accepted ((,class (:inherit sx-question-list-answers :weight bold :underline t))))
-     `(sx-question-list-bounty ((,class (:inherit font-lock-warning-face))))
+     `(sx-question-list-bounty ((,class (:foreground ,cyan))))
      `(sx-question-list-date ((,class (:inherit font-lock-comment-face))))
      `(sx-question-list-favorite ((,class (:inherit sx-question-list-score-upvoted))))
      `(sx-question-list-parent ((,class (:inherit default))))
      `(sx-question-list-read-question ((,class (:inherit sx-question-list-parent :height 1.0))))
-     `(sx-question-list-score ((,class (:inherit sx-question-list-parent :height 1.0))))
+     `(sx-question-list-score ((,class (:inherit sx-question-list-parent :foreground ,base01 :height 1.0))))
      `(sx-question-list-score-upvoted ((,class (:inherit sx-question-list-score :weight bold))))
      `(sx-question-list-unread-question ((,class (:inherit sx-question-list-read-question :weight bold))))
      `(sx-question-mode-accepted ((,class (:inherit sx-question-mode-title :foreground ,green :height 1.3))))
      `(sx-question-mode-closed ((,class (:inherit font-lock-warning-face :box 2))))
-     `(sx-question-mode-closed-reason ((,class (:inherit sx-question-mode-title :box (:line-width 2 :color ,orange)))))
+     `(sx-question-mode-closed-reason ((,class (:inherit sx-question-mode-title :box (:line-width 2 :color ,yellow)))))
+     ;; TODO: sx-question-mode-content-faceexposes a general problem that's hard to deal with,
+     ;; if base02 is used as bg some things are not visible enough.. It might be a good idea to
+     ;; introduce yet another special color that goes a little furhter towards netural gray and
+     ;; ensures readability as a bg for all solarized faces. If it's possible, that is.
      `(sx-question-mode-content-face ((,class (:background unspecified))))
      `(sx-question-mode-date ((,class (:inherit font-lock-string-face))))
      `(sx-question-mode-header ((,class (:inherit message-header-name :weight normal))))
-     ;; `(sx-question-mode-kbd-tag ((,class (:box (:line-width 3 :style released-button :color "gray70") :weight semibold :height 0.9))))
+     `(sx-question-mode-kbd-tag ((,class (:box (:line-width 3 :style released-button :color ,base02) :weight semibold :height 0.9))))
      `(sx-question-mode-score ((,class nil)))
      `(sx-question-mode-score-downvoted ((,class (:inherit (font-lock-warning-face sx-question-mode-score)))))
      `(sx-question-mode-score-upvoted ((,class (:inherit (font-lock-function-name-face sx-question-mode-score) :weight bold))))
