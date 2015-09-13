473c531fe6bd7cab30e63d8ae81dde49ef5b09e1
PythonNut <PythonNut@users.noreply.github.com>
2015-09-13 14:43:36 -0700

    Fix typo in child theme examples

--- child-theme-example/themes/my-solarized-dark-theme.el
+++ child-theme-example/themes/my-solarized-dark-theme.el
@@ -3,7 +3,7 @@
 
 ;; This files needs to be places iside the custom-theme-load-path list
 
-(deftheme my-solarized-dark "The light variant of the Solarized colour theme")
+(deftheme my-solarized-dark "The dark variant of the Solarized colour theme")
 (create-solarized-theme 'dark 'my-solarized-dark 'my-solarized-theme)
 
 (provide-theme 'my-solarized-dark)
