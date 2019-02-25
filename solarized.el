56b3be7609d75747a6548a80ded24cfc3e6d7e92
Emmanuel de Jesus Bustos Torres <emmanuel-de-jesus.bustos@hpe.com>
2019-02-24 23:05:43 -0600

    Added custom faces for modified tabs

--- solarized.el
+++ solarized.el
@@ -2034,13 +2034,15 @@ customize the resulting theme."
      `(sr-clex-hotchar-face ((,class (:background ,red  :foreground ,base03
                                                   :weight bold))))
 ;;;;; tabbar
-     `(tabbar-button ((,class (:inherit tabbar-default ))))
+     `(tabbar-button ((,class (:inherit tabbar-default))))
      `(tabbar-button-highlight ((,class (:inherit tabbar-default))))
      `(tabbar-default ((,class (:background ,base03 :foreground ,base3 :height 0.9))))
      `(tabbar-highlight ((,class (:underline t))))
      `(tabbar-selected ((,class (:inherit tabbar-default :background ,base03 :foreground ,base3 :weight bold :height 140))))
+     `(tabbar-selected-modified ((,class (:inherit tabbar-default :background ,base03 :foreground ,blue :weight bold :height 140))))
      `(tabbar-separator ((,class (:inherit tabbar-default))))
      `(tabbar-unselected ((,class (:inherit tabbar-default :background ,base02 :weight light :slant italic :height 140))))
+     `(tabbar-modified ((,class (:inherit tabbar-default :background ,base02 :foreground ,blue :weight light :slant italic :height 140))))
 
 ;;;;; table
      `(table-cell ((,class (:foreground ,base0 :background ,base02))))
