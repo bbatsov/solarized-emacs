092628d6bcde3973e3439ba56158790fcd381b87
Thomas Frössman <thomasf@jossystem.se>
2019-11-03 22:10:41 +0100

    add script that creates a repo with merge conflict

---
+++ minimal-init/create-merge-conflict-repo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+set -x
+
+rm -rf test-repo
+mkdir -p test-repo
+cd test-repo
+git init .
+touch HELLO
+git add HELLO
+git commit -m"hello"
+
+
+get_files(){
+  local version=${1}
+  shift
+  for f in pdb.py tempfile.py os.py inspect.py mimetypes.py; do
+    curl -o "${f}" "https://raw.githubusercontent.com/python/cpython/${version}/Lib/${f}"
+  done
+  git add -A
+  git commit -m"version ${v}"
+}
+
+git checkout master
+git checkout -b v1
+get_files v3.4.8
+git checkout -b v2
+git checkout -b v3
+git checkout v1
+get_files v3.5.0
+get_files v3.6.0
+git checkout v2
+get_files v3.7.0
+git checkout v3
+get_files v3.8.0
+git checkout master
+
+git checkout v2
+
+git merge v3 || true
+
+
+git diff --color | cat
