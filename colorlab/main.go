4d43e82e8ae99dc4a56fc60ec9c41e1a2b35a545
Thomas Frössman <thomasf@jossystem.se>
2019-11-17 16:57:15 +0100

    colorlab: move color constants to own package

--- colorlab/main.go
+++ colorlab/main.go
@@ -14,6 +14,7 @@ import (
 
 	"bytes"
 
+	"github.com/bbatsov/solarized-emacs/colorlab/pkg/colors"
 	colorful "github.com/lucasb-eyer/go-colorful"
 )
 
@@ -667,24 +668,24 @@ func Merge(nc ...NamedColors) NamedColors {
 var ( // The original solarized color palette
 	solarized = Solarized{
 		Base: Base{
-			Base03: SolarizedBase03,
-			Base02: SolarizedBase02,
-			Base01: SolarizedBase01,
-			Base00: SolarizedBase00,
-			Base0:  SolarizedBase0,
-			Base1:  SolarizedBase1,
-			Base2:  SolarizedBase2,
-			Base3:  SolarizedBase3,
+			Base03: colors.SolarizedBase03,
+			Base02: colors.SolarizedBase02,
+			Base01: colors.SolarizedBase01,
+			Base00: colors.SolarizedBase00,
+			Base0:  colors.SolarizedBase0,
+			Base1:  colors.SolarizedBase1,
+			Base2:  colors.SolarizedBase2,
+			Base3:  colors.SolarizedBase3,
 		},
 		Accents: Accents{
-			Blue:    SolarizedBlue,
-			Cyan:    SolarizedCyan,
-			Green:   SolarizedGreen,
-			Magenta: SolarizedMagenta,
-			Orange:  SolarizedOrange,
-			Red:     SolarizedRed,
-			Violet:  SolarizedViolet,
-			Yellow:  SolarizedYellow,
+			Blue:    colors.SolarizedBlue,
+			Cyan:    colors.SolarizedCyan,
+			Green:   colors.SolarizedGreen,
+			Magenta: colors.SolarizedMagenta,
+			Orange:  colors.SolarizedOrange,
+			Red:     colors.SolarizedRed,
+			Violet:  colors.SolarizedViolet,
+			Yellow:  colors.SolarizedYellow,
 		},
 	}
 	solarizedDarkHighContrast = Solarized{
@@ -720,214 +721,94 @@ var ( // The original solarized color palette
 	}
 	gruvboxDark = Solarized{
 		Base: Base{
-			Base03: GruvboxDark0,
-			Base02: GruvboxDark0Soft,
-			Base01: GruvboxDark4,
-			Base00: GruvboxDark0,
-			Base0:  GruvboxLight4,
-			Base1:  GruvboxLight3,
-			Base2:  GruvboxLight4,
-			Base3:  GruvboxLight0,
+			Base03: colors.GruvboxDark0,
+			Base02: colors.GruvboxDark0Soft,
+			Base01: colors.GruvboxDark4,
+			Base00: colors.GruvboxDark0,
+			Base0:  colors.GruvboxLight4,
+			Base1:  colors.GruvboxLight3,
+			Base2:  colors.GruvboxLight4,
+			Base3:  colors.GruvboxLight0,
 		},
 		Accents: Accents{
-			Blue:    GruvboxBlue,
-			Cyan:    GruvboxAqua,
-			Green:   GruvboxGreen,
-			Magenta: GruvboxBrightPurple,
-			Orange:  GruvboxOrange,
-			Red:     GruvboxBrightRed,
-			Violet:  GruvboxPurple,
-			Yellow:  GruvboxYellow,
+			Blue:    colors.GruvboxBlue,
+			Cyan:    colors.GruvboxAqua,
+			Green:   colors.GruvboxGreen,
+			Magenta: colors.GruvboxBrightPurple,
+			Orange:  colors.GruvboxOrange,
+			Red:     colors.GruvboxBrightRed,
+			Violet:  colors.GruvboxPurple,
+			Yellow:  colors.GruvboxYellow,
 		},
 	}
 	// note: not inversed here
 	gruvboxLight = Solarized{
 		Base: Base{
-			Base03: GruvboxDark0,
-			Base02: GruvboxDark0Soft,
-			Base01: GruvboxDark3,
-			Base00: GruvboxDark4,
-			Base0:  GruvboxDark1,
-			Base1:  GruvboxLight4,
-			Base2:  GruvboxLight1,
-			Base3:  GruvboxLight0,
+			Base03: colors.GruvboxDark0,
+			Base02: colors.GruvboxDark0Soft,
+			Base01: colors.GruvboxDark3,
+			Base00: colors.GruvboxDark4,
+			Base0:  colors.GruvboxDark1,
+			Base1:  colors.GruvboxLight4,
+			Base2:  colors.GruvboxLight1,
+			Base3:  colors.GruvboxLight0,
 		},
 		Accents: Accents{
-			Blue:    GruvboxDarkBlue,
-			Cyan:    GruvboxAqua,
-			Green:   GruvboxGreen,
-			Magenta: GruvboxBrightPurple,
-			Orange:  GruvboxDarkOrange,
-			Red:     GruvboxDarkRed,
-			Violet:  GruvboxDarkPurple,
-			Yellow:  GruvboxDarkYellow,
+			Blue:    colors.GruvboxDarkBlue,
+			Cyan:    colors.GruvboxAqua,
+			Green:   colors.GruvboxGreen,
+			Magenta: colors.GruvboxBrightPurple,
+			Orange:  colors.GruvboxDarkOrange,
+			Red:     colors.GruvboxDarkRed,
+			Violet:  colors.GruvboxDarkPurple,
+			Yellow:  colors.GruvboxDarkYellow,
 		},
 	}
 	zenburn = Solarized{
 		Base: Base{
-			Base03: ZenburnBg,
-			Base02: ZenburnBgP1,
-			Base01: HexColor(ZenburnFgM1).Blend(ZenburnFg, 0.3),
-			Base00: ZenburnBgP3,
-			Base0:  ZenburnFg,
-			Base1:  ZenburnFgP1,
-			Base2:  HexColor(ZenburnFgP1).Blend(ZenburnFgP2, 0.5),
-			Base3:  ZenburnFgP2,
+			Base03: colors.ZenburnBg,
+			Base02: colors.ZenburnBgP1,
+			Base01: HexColor(colors.ZenburnFgM1).Blend(colors.ZenburnFg, 0.3),
+			Base00: colors.ZenburnBgP3,
+			Base0:  colors.ZenburnFg,
+			Base1:  colors.ZenburnFgP1,
+			Base2:  HexColor(colors.ZenburnFgP1).Blend(colors.ZenburnFgP2, 0.5),
+			Base3:  colors.ZenburnFgP2,
 		},
 		Accents: Accents{
-			Blue:    ZenburnBlue,
-			Cyan:    ZenburnCyan,
-			Green:   ZenburnGreen,
-			Magenta: ZenburnMagenta,
-			Orange:  ZenburnOrange,
-			Red:     ZenburnRed,
-			Violet:  HexColor(ZenburnBlue).Blend(ZenburnMagenta, 0.5),
-			Yellow:  ZenburnYellow,
+			Blue:    colors.ZenburnBlue,
+			Cyan:    colors.ZenburnCyan,
+			Green:   colors.ZenburnGreen,
+			Magenta: colors.ZenburnMagenta,
+			Orange:  colors.ZenburnOrange,
+			Red:     colors.ZenburnRed,
+			Violet:  HexColor(colors.ZenburnBlue).Blend(colors.ZenburnMagenta, 0.5),
+			Yellow:  colors.ZenburnYellow,
 		},
 	}
 
 	monokai = Solarized{
 		Base: Base{
-			Base03: Monokai03,
-			Base02: Monokai02,
-			Base01: Monokai01,
-			Base00: Monokai00,
-			Base0:  Monokai0,
-			Base1:  Monokai0,
-			Base2:  Monokai0,
-			Base3:  Monokai0,
+			Base03: colors.Monokai03,
+			Base02: colors.Monokai02,
+			Base01: colors.Monokai01,
+			Base00: colors.Monokai00,
+			Base0:  colors.Monokai0,
+			Base1:  colors.Monokai0,
+			Base2:  colors.Monokai0,
+			Base3:  colors.Monokai0,
 		},
 		Accents: Accents{
-			Blue:    MonokaiBlue,
-			Cyan:    MonokaiCyan,
-			Green:   MonokaiGreen,
-			Magenta: MonokaiMagenta,
-			Orange:  MonokaiOrange,
-			Red:     MonokaiRed,
-			Violet:  MonokaiViolet,
-			Yellow:  MonokaiYellow,
+			Blue:    colors.MonokaiBlue,
+			Cyan:    colors.MonokaiCyan,
+			Green:   colors.MonokaiGreen,
+			Magenta: colors.MonokaiMagenta,
+			Orange:  colors.MonokaiOrange,
+			Red:     colors.MonokaiRed,
+			Violet:  colors.MonokaiViolet,
+			Yellow:  colors.MonokaiYellow,
 		},
 	}
-)
-
-const (
-	SolarizedBase03  = "#002b36"
-	SolarizedBase02  = "#073642"
-	SolarizedBase01  = "#586e75"
-	SolarizedBase00  = "#657b83"
-	SolarizedBase0   = "#839496"
-	SolarizedBase1   = "#93a1a1"
-	SolarizedBase2   = "#eee8d5"
-	SolarizedBase3   = "#fdf6e3"
-	SolarizedBlue    = "#268bd2"
-	SolarizedCyan    = "#2aa198"
-	SolarizedGreen   = "#859900"
-	SolarizedMagenta = "#d33682"
-	SolarizedOrange  = "#cb4b16"
-	SolarizedRed     = "#dc322f"
-	SolarizedViolet  = "#6c71c4"
-	SolarizedYellow  = "#b58900"
-
-	GruvboxDark0Hard    = "#1d2021"
-	GruvboxDark0        = "#282828"
-	GruvboxDark0Soft    = "#32302f"
-	GruvboxDark1        = "#3c3836"
-	GruvboxDark2        = "#504945"
-	GruvboxDark3        = "#665c54"
-	GruvboxDark4        = "#7c6f64"
-	GruvboxGray         = "#928374"
-	GruvboxLight0Hard   = "#f9f5d7"
-	GruvboxLight0       = "#fbf1c7"
-	GruvboxLight0Soft   = "#f2e5bc"
-	GruvboxLight1       = "#ebdbb2"
-	GruvboxLight2       = "#d5c4a1"
-	GruvboxLight3       = "#bdae93"
-	GruvboxLight4       = "#a89984"
-	GruvboxRed          = "#cc241d"
-	GruvboxGreen        = "#98971a"
-	GruvboxYellow       = "#d79921"
-	GruvboxBlue         = "#458588"
-	GruvboxPurple       = "#b16286"
-	GruvboxAqua         = "#689d6a"
-	GruvboxOrange       = "#d65d0e"
-	GruvboxBrightRed    = "#fb4933"
-	GruvboxBrightGreen  = "#b8bb26"
-	GruvboxBrightYellow = "#fabd2f"
-	GruvboxBrightBlue   = "#83a598"
-	GruvboxBrightPurple = "#d3869b"
-	GruvboxBrightAqua   = "#8ec07c"
-	GruvboxBrightOrange = "#fe8019"
-	GruvboxDarkRed      = "#9d0006"
-	GruvboxDarkGreen    = "#79740e"
-	GruvboxDarkYellow   = "#b57614"
-	GruvboxDarkBlue     = "#076678"
-	GruvboxDarkPurple   = "#8f3f71"
-	GruvboxDarkAqua     = "#427b58"
-	GruvboxDarkOrange   = "#af3a03"
-
-	ZenburnFgM1     = "#656555"
-	ZenburnFgM05    = "#989890"
-	ZenburnFg       = "#DCDCCC"
-	ZenburnFgP1     = "#FFFFEF"
-	ZenburnFgP2     = "#FFFFFD"
-	ZenburnBgM2     = "#000000"
-	ZenburnBgM1     = "#2B2B2B"
-	ZenburnBgM08    = "#303030"
-	ZenburnBgM05    = "#383838"
-	ZenburnBg       = "#3F3F3F"
-	ZenburnBgP05    = "#494949"
-	ZenburnBgP1     = "#4F4F4F"
-	ZenburnBgP2     = "#5F5F5F"
-	ZenburnBgP3     = "#6F6F6F"
-	ZenburnRedM6    = "#6C3333"
-	ZenburnRedM5    = "#7C4343"
-	ZenburnRedM4    = "#8C5353"
-	ZenburnRedM3    = "#9C6363"
-	ZenburnRedM2    = "#AC7373"
-	ZenburnRedM1    = "#BC8383"
-	ZenburnRed      = "#CC9393"
-	ZenburnRedP1    = "#DCA3A3"
-	ZenburnRedP2    = "#ECB3B3"
-	ZenburnOrange   = "#DFAF8F"
-	ZenburnYellowM2 = "#D0BF8F"
-	ZenburnYellowM1 = "#E0CF9F"
-	ZenburnYellow   = "#F0DFAF"
-	ZenburnGreenM5  = "#2F4F2F"
-	ZenburnGreenM4  = "#3F5F3F"
-	ZenburnGreenM3  = "#4F6F4F"
-	ZenburnGreenM2  = "#5F7F5F"
-	ZenburnGreenM1  = "#6F8F6F"
-	ZenburnGreen    = "#7F9F7F"
-	ZenburnGreenP1  = "#8FB28F"
-	ZenburnGreenP2  = "#9FC59F"
-	ZenburnGreenP3  = "#AFD8AF"
-	ZenburnGreenP4  = "#BFEBBF"
-	ZenburnCyan     = "#93E0E3"
-	ZenburnBlueP3   = "#BDE0F3"
-	ZenburnBlueP2   = "#ACE0E3"
-	ZenburnBlueP1   = "#94BFF3"
-	ZenburnBlue     = "#8CD0D3"
-	ZenburnBlueM1   = "#7CB8BB"
-	ZenburnBlueM2   = "#6CA0A3"
-	ZenburnBlueM3   = "#5C888B"
-	ZenburnBlueM4   = "#4C7073"
-	ZenburnBlueM5   = "#366060"
-	ZenburnMagenta  = "#DC8CC3"
-
-	// ehm, monokaiXX are probably not "correct" in any way
-	Monokai03      = "#272822"
-	Monokai02      = "#3E3D31"
-	Monokai01      = "#75715E"
-	Monokai00      = "#49483E"
-	Monokai0       = "#F8F8F2"
-	MonokaiYellow  = "#E6DB74"
-	MonokaiOrange  = "#FD971F"
-	MonokaiRed     = "#F92672"
-	MonokaiMagenta = "#FD5FF0"
-	MonokaiBlue    = "#66D9EF"
-	MonokaiGreen   = "#A6E22E"
-	MonokaiCyan    = "#A1EFE4"
-	MonokaiViolet  = "#AE81FF"
 )
 
 // Palette .
