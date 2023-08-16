07a681fff27ff22d268b726b05576ef93fc4e615
Ruslan Kamashev <rynffoll@gmail.com>
2023-08-16 21:32:58 +0400

    Add corfu face (#442)
    Add corfu face

--- solarized-faces.el
+++ solarized-faces.el
@@ -433,6 +433,11 @@
      `(company-tooltip-search ((,class (:foreground ,magenta))))
      `(company-tooltip-search-selection ((,class (:foreground ,magenta :weight bold))))
      `(company-tooltip-selection ((,class (:weight bold))))
+;;;;; corfu
+     `(corfu-default ((,class (:foreground ,base1 :background ,base02))))
+     `(corfu-current ((,class (:inherit corfu-default :weight bold :inverse-video t))))
+     `(corfu-bar ((,class (:background ,base0))))
+     `(corfu-border ((,class (:background ,base02))))
 ;;;;; consult
      `(consult-preview-insertion ((,class (:background ,base02))))
      `(consult-preview-line ((,class (:background ,base02))))
